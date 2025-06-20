5454e77201af365df8ef1688b110beb8309e8123
Mahen Abeywickrama <mahenabeywickrema0@gmail.com>
2025-06-20 17:24:46 +0530

    added complaint servlet

---
+++ src/main/java/com/example/controller/SubmitComplaintServlet.java
@@ -0,0 +1,45 @@
+package com.example.controller;
+
+import com.example.dao.ComplaintDAO;
+import com.example.model.Complaint;
+import com.example.model.User;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.io.IOException;
+
+@WebServlet("/submitComplaint")
+public class SubmitComplaintServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            resp.sendRedirect("jsp/login.jsp");
+            return;
+        }
+
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource ds = (BasicDataSource) servletContext.getAttribute("ds");
+
+        ComplaintDAO complaintDAO = new ComplaintDAO(ds);
+
+        User user = (User) session.getAttribute("user");
+
+        String title = req.getParameter("title");
+        String description = req.getParameter("description");
+
+        Complaint complaint = new Complaint();
+        complaint.setUserId(user.getId());
+        complaint.setTitle(title);
+        complaint.setDescription(description);
+
+        complaintDAO.addComplaint(complaint);
+        resp.sendRedirect("viewMyComplaints");
+    }
+}
